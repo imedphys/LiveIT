15fc1599352d4f2ad4655d24c0cc257dedd28d06
Giorgos Ntakakis <gntakakis@outlook.com>
2022-10-21 17:09:48 +0300

    Revert "upload project"
    This reverts commit 0940b1da3aee64ce4c4ab43d33ed38778be12eda.

--- LiveITAPI/Models/CatalogueTypeModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace LiveITAPI.Models
-{
-    public class CatalogueTypeModel
-    {
-        public int CatalogueTypeId { get; set; }
-        public string Name { get; set; }
-    }
-}
--- LiveITAPI/Models/StakeholderTypeModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace LiveITAPI.Models
-{
-    public class StakeholderTypeModel
-    {
-        public int StakeholderTypeId { get; set; }
-        public string Name { get; set; }
-    }
-}
